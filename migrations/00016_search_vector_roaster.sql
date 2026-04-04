c560269ea3abe66fa7ace17d56ab91d8c41585e9
thrgamon <tom@tomgamon.com>
2026-04-04 14:23:03 +1100

    Include roaster name in coffee search vector for full-text search

---
+++ migrations/00016_search_vector_roaster.sql
@@ -0,0 +1,32 @@
+-- +goose Up
+CREATE OR REPLACE FUNCTION coffees_search_vector_update() RETURNS trigger AS $$
+DECLARE
+    roaster_name TEXT;
+BEGIN
+    SELECT name INTO roaster_name FROM roasters WHERE id = NEW.roaster_id;
+    NEW.search_vector :=
+        setweight(to_tsvector('english', coalesce(NEW.name, '')), 'A') ||
+        setweight(to_tsvector('english', coalesce(roaster_name, '')), 'A') ||
+        setweight(to_tsvector('english', coalesce(NEW.origin_raw, '')), 'B') ||
+        setweight(to_tsvector('english', coalesce(NEW.tasting_notes_raw, '')), 'C') ||
+        setweight(to_tsvector('english', coalesce(NEW.variety_raw, '')), 'D');
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+-- Rebuild all search vectors to include roaster names
+UPDATE coffees SET updated_at = updated_at;
+
+-- +goose Down
+CREATE OR REPLACE FUNCTION coffees_search_vector_update() RETURNS trigger AS $$
+BEGIN
+    NEW.search_vector :=
+        setweight(to_tsvector('english', coalesce(NEW.name, '')), 'A') ||
+        setweight(to_tsvector('english', coalesce(NEW.origin_raw, '')), 'B') ||
+        setweight(to_tsvector('english', coalesce(NEW.tasting_notes_raw, '')), 'C') ||
+        setweight(to_tsvector('english', coalesce(NEW.variety_raw, '')), 'D');
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+UPDATE coffees SET updated_at = updated_at;
